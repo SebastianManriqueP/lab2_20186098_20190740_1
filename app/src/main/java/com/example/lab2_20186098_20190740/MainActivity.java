999f2bf4585c55feb2b4286f5c9430d71049d82c
Sebastian <a20186098@pucp.edu.pe>
2023-04-24 14:34:32 -0500

    Pregunta 1 parte 2

--- app/src/main/java/com/example/lab2_20186098_20190740/MainActivity.java
+++ app/src/main/java/com/example/lab2_20186098_20190740/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.lab2_20186098_20190740;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, detalleEvento.class);
+            launcher.launch(intent);
+        });
     }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent resultData = result.getData();
+                    String textoDevolver = resultData.getStringExtra("textoDevolver");
+                    Toast.makeText(MainActivity.this, "Texto: " + textoDevolver, Toast.LENGTH_SHORT).show();
+                }
+            }
+    );
+
 }
--- app/src/main/java/com/example/lab2_20186098_20190740/detalleEvento.java
+++ app/src/main/java/com/example/lab2_20186098_20190740/detalleEvento.java
@@ -3,6 +3,8 @@ package com.example.lab2_20186098_20190740;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class detalleEvento extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class detalleEvento extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_evento);
 
+
+        ImageButton imagenBtn = findViewById(R.id.imageViewBtn);
+        imagenBtn.setOnClickListener(view -> {
+            onBackPressed();
+        });
     }
 
 
